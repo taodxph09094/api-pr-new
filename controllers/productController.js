f6ceb5dfe203d3f2c9286edccb6bc7dfc94be0bc
Tao dang <taodxph09094@gmail.com>
2023-05-24 18:51:11 +0700

    .

--- controllers/productController.js
+++ controllers/productController.js
@@ -66,11 +66,33 @@ exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
 
 // Get All Product (Admin)
 exports.getAdminProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find();
+  // const products = await Product.find();
+
+  // res.status(200).json({
+  //   success: true,
+  //   products,
+  // });
+  const resultPerPage = 8;
+  const productsCount = await Product.countDocuments();
+
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter();
+
+  let products = await apiFeature.query;
+
+  let filteredProductsCount = products.length;
+
+  apiFeature.pagination(resultPerPage);
+
+  products = await apiFeature.query;
 
   res.status(200).json({
     success: true,
     products,
+    productsCount,
+    resultPerPage,
+    filteredProductsCount,
   });
 });
 
